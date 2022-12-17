ecd43c8ce834652b8c2759f5803d406af0ece7e6
Soliman Family <AhmedMohammedSoliman>
2022-12-17 12:38:20 +0200

    begin news project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'home/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName : (context) => HomeScreen(),
+      },
+      locale: Locale("en"),
+    );
+  }
+}
+
